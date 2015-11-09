499b78a599b7cc80f8536ee58b78be365b52394a
Adolfo Correa <fitomillennium@hotmail.com>
2015-11-09 17:33:08 -0200

    Partial calculus of the Standard deviation
    the algorithm carrying a vector with random numbers and during charging,
    it calculates the mean and standard deviation of the values

---
+++ std1.m
@@ -0,0 +1,25 @@
+temp = 0;
+v(1)=rand()*1000;
+v(2) = rand()*1000;
+media_parcial = (v(2)+v(2-1))/2
+var = (v(2)^2+v(2-1)^2)/2 - media_parcial^2
+for i= 3:1:1000
+    v(i) = rand()*1000;
+%     media_parcial = (v(i)+v(i-1))/i
+    var = var + media_parcial^2;
+    media_parcial = (media_parcial*(i-1)+v(i))/i
+%     var = (v(i)^2+v(i-1)^2)/i
+    var = (var*(i-1)+v(i)^2)/i - media_parcial^2
+end
+media_parcial
+var
+media = mean(v)
+desvio = std(v)^2
+
+varianza = 0;
+for i = 1:1000
+    varianza = varianza + (v(i)-media)^2;
+end
+i
+varianza = varianza/(i-1)
+varianza = varianza*(i-1)/i
