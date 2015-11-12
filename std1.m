68e01579a97e1eacec8d90143404e83acf0a7f30
Adolfo Correa <fitomillennium@hotmail.com>
2015-11-11 22:08:53 -0200

    Variance n-1 and n

--- std1.m
+++ std1.m
@@ -1,9 +1,10 @@
+N = 1000
 temp = 0;
 v(1)=rand()*1000;
 v(2) = rand()*1000;
 media_parcial = (v(2)+v(2-1))/2
 var = (v(2)^2+v(2-1)^2)/2 - media_parcial^2
-for i= 3:1:1000
+for i= 3:1:N
     v(i) = rand()*1000;
 %     media_parcial = (v(i)+v(i-1))/i
     var = var + media_parcial^2;
@@ -17,9 +18,9 @@ media = mean(v)
 desvio = std(v)^2
 
 varianza = 0;
-for i = 1:1000
+for i = 1:N
     varianza = varianza + (v(i)-media)^2;
 end
 i
-varianza = varianza/(i-1)
-varianza = varianza*(i-1)/i
+varianza_n_1 = varianza/(i-1)
+varianza_n = varianza/i
